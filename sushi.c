ffda18aa03393e333bdb7adfecabe0bae677cfe7
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-01-03 17:47:22 +0900

    Support multiple sushi supplement on each line

--- sushi.c
+++ sushi.c
@@ -20,19 +20,24 @@ static char* msg = "module [sushi.o]";
 
 static struct cdev cdv;
 static int access_num = 0;
+static int num_per_line = 1; //the number of sushi written in each line
 static spinlock_t spn_lock;
 
 static struct class *cls = NULL;
 
 static int sushi_open(struct inode* inode, struct file* filp);
-static ssize_t sushi_read(struct file* filp, const char* buf, size_t count, loff_t* pos);
+static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos);
+static ssize_t sushi_write(struct file* filp, const char* buf, size_t count, loff_t* pos);
 static int sushi_release(struct inode* inode, struct file* filp);
 static int get_pseudo_rand(void);
+
+static int copy_to_user_n(char *buf,const char *str,int str_len,int n);
 
 static struct file_operations sushi_fops = 
 {
 	owner   : THIS_MODULE,
 	read    : sushi_read,
+	write    : sushi_write,
 	open    : sushi_open,
 	release : sushi_release,
 };
@@ -61,38 +66,71 @@ static int sushi_open(struct inode* inode, struct file* filp)
 	return 0;
 }
 
-static ssize_t sushi_read(struct file* filp, const char* buf, size_t count, loff_t* pos)
+static int copy_to_user_n(char *buf,const char *str,int str_len,int n)
+{
+	int size = 0;
+	int i;
+	for(i=0;i<n;i++){
+		if(copy_to_user(buf+size,str, str_len)){
+			printk( KERN_INFO "%s : copy_to_user failed\n", msg );
+			return -EFAULT;
+		}
+
+		size += str_len;
+		if(i>10)
+			return size;
+	}
+
+	return size;
+}
+
+static ssize_t sushi_read(struct file* filp, char* buf, size_t count, loff_t* pos)
 {
 	static unsigned int n = 0;
 	int ret = 0;
 	int size = 0;
+	int tmp;
+	int i;
 
-	char sushi[] = {0xF0,0x9F,0x8D,0xA3,'\n','\0'};
-	char sushi_beer[] = {0xF0,0x9F,0x8D,0xA3,' ',0xF0,0x9F,0x8D,0xBA,'\n','\0'};
+	char sushi[] = {0xF0,0x9F,0x8D,0xA3,'\0'};
+	char sushi_beer[] = {0xF0,0x9F,0x8D,0xA3,' ',0xF0,0x9F,0x8D,0xBA,'\0'};
+	char nl[] = {'\n','\0'};
 
 	if(n%10){
-		size = sizeof(sushi);
-		ret = copy_to_user(buf,(const char *)sushi, size);
+		ret = copy_to_user_n(buf+size,(const char *)sushi,
+					sizeof(sushi),num_per_line);
 	}else{
-		size = sizeof(sushi_beer);
-		ret = copy_to_user(buf,(const char *)sushi_beer, size);
+		ret = copy_to_user_n(buf+size,(const char *)sushi_beer,
+					sizeof(sushi_beer),num_per_line);
+	}
+	if(ret == -EFAULT){
+		return -EFAULT;
 	}
 
-	if(ret){
+	size += ret;
+
+	if(copy_to_user(buf+size,(const char *)nl, sizeof(nl))){
 		printk( KERN_INFO "%s : copy_to_user failed\n", msg );
 		return -EFAULT;
 	}
+	size += sizeof(nl);
 
 	n++;
 	return size;
 }
 
-/*
 static ssize_t sushi_write(struct file* filp, const char* buf, size_t count, loff_t* pos)
 {
-	return 0;
+	char c;
+	if(copy_from_user(&c,buf,sizeof(char))){
+		return -EFAULT;
+	}
+	if(c >= '0' && c <= '9'){
+		num_per_line = (int)c - 48;
+	}
+
+	return 1;
 }
-*/
 
 static int sushi_release(struct inode* inode, struct file* filp)
 {
