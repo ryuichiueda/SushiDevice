ba40f782029ac4b35c6ea114c6a600191753ba52
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-01-03 15:57:44 +0900

    Remove redundant files

--- .sushi.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ueda/GIT/SushiDevice/sushi.ko := ld -r -m elf_x86_64 -T /usr/src/linux-headers-3.13.0-24-generic/scripts/module-common.lds --build-id  -o /home/ueda/GIT/SushiDevice/sushi.ko /home/ueda/GIT/SushiDevice/sushi.o /home/ueda/GIT/SushiDevice/sushi.mod.o
